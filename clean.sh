0ab6051cfeb1268f07e51d64d3958dec24c26a01
Harou <haroutioun24@gmail.com>
2021-02-07 13:04:56 +0100

    setup.sh clean.sh

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+kubectl delete --all ingresses
+kubectl delete --all deployments
+kubectl delete --all pods
+kubectl delete --all services
+kubectl delete --all pvc
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Starting minikube ->"
+minikube --vm-driver=virtual-box
+minikube start
+
+echo "Enable ingress and dashboard"
+minikube addons enable ingress
+minikube addons enable dashboard
+
+echo "Dashboard ->"
+minikube dashboard &
+
+echo "Eval -> "
+eval $(minikube docker-env)
+IP=$(minikube ip)
+printf "Minikube IP: ${IP}"
+
+echo "Building images ->"
+docker build -t nginx ./srcs/nginx
+
+echo "Creating pods and services ->"
+kubectl create -f ./srcs/
